c86ff4b4cf1138ffe7e27d061e316e36765ec2ee
Gustavs Fricis Jankavs <gfjankavs@tutamail.com>
2024-03-24 13:11:15 +0200

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <div className="content">
+            <h1>Password generator</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
